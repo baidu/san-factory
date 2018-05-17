89b0c4dd7234c13552070ad75140747b5249040d
errorrik <errorrik@gmail.com>
2018-05-17 16:55:56 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@
      * 创建组件实例
      *
      * @param {Object} instanceConfig 实例创建的配置对象
+     * @param {string} instanceConfig.component 组件类名称，与factoryConfig.components的key对应
      * @param {Object?} instanceConfig.options 实例创建时的参数
      * @param {Object?} instanceConfig.options.data 实例创建的初始化数据
      * @param {HTMLElement?} instanceConfig.options.el 实例创建时的主元素，组件反解时用
